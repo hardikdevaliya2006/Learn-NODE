8d81e0f444cbcdc4a4bd679a0ae9cecc098bfcc3
hardikdevaliya2006] <hardikdevaliya2006@gmail.com>
2025-10-18 20:18:38 +0530

    Add mongoose dependency and update routes in index.js; clean up home.ejs

--- Projects/CRUD/index.js
+++ Projects/CRUD/index.js
@@ -1,38 +1,41 @@
-import express from 'express'
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
 
-app.use(express.urlencoded({extended: false}))
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+// Data base Connecation
 
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Routes
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/show-data', (req, res) => {
-   res.render('show-data')
-})
+// Show
+app.get("/show-data", (req, res) => {
+  res.render("show-data");
+});
 
-app.get('/add-data', (req, res) => {
-  res.render('add-data')
-})
+// Add Data
+app.get("/add-data", (req, res) => {
+  res.render("add-data");
+});
 
-app.post('/add-data', (req, res) => {
+app.post("/add-data", (req, res) => {});
 
-})
+// Update Data
+app.get("/update-data", (req, res) => {
+  res.render("update-data");
+});
 
-app.get('/update-data', (req, res) => {
-  res.render('update-data')
-})
+app.post("/update-data", (req, res) => {});
 
-app.post('/update-data', (req, res) => {
-
-})
-
-app.get('/delete-data', (req, res) => {
-
-})
+// Delete Data
+app.get("/delete-data", (req, res) => {});
