b1db0d9d785c5a7b8c4fcfe6722d1d8d0d93cfc4
hardikdevaliya2006] <hardikdevaliya2006@gmail.com>
2025-10-18 16:28:58 +0530

    Add form handling and new views for user input and profile display

--- 04-Template-Engine/02-EJS-code/index.js
+++ 04-Template-Engine/02-EJS-code/index.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 3000;
 
 app.set("view engine", "ejs");
-app.listen(3000, () => {
+app.use(express.urlencoded({ extended: false }));
+app.listen(PORT, () => {
   console.log("App running on http://localhost:3000");
 });
 
@@ -12,7 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/index", (req, res) => {
-    const language = ['js', 'react.js', 'node.js', 'C', 'C++']
+  const language = ["js", "react.js", "node.js", "C", "C++"];
   res.render("index", {
     title: "This is Home Page",
     userName: "hardikdevaliya2006",
@@ -20,4 +21,14 @@ app.get("/index", (req, res) => {
     message:
       "You Are Located At the home Page Of the Our Site Localhost : 3000",
   });
+});
+
+app.get("/from", (req, res) => {
+  res.render("from", {message: null});
+});
+
+app.post("/submit", (req, res) => {
+  const { username, email } = req.body;
+  const message = `Wellcome To Our Website, ${username} (${email})`;
+  res.render("from", {message})
 });
